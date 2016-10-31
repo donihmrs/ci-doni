2e362035231e452b7d54b0aedc78fae80990c10f
Doni-PC\Doni <donihamster88@gmail.com>
2016-11-01 01:02:06 +0700

    perubahan view

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://bekup-doni.herokuapp.com/';
+$config['base_url'] = 'https://bekup-doni.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
